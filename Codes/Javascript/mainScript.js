7361fb6dbd0bac7a28c9a6e162d93b61a06c6292
Joel Henry <85650415+MacaroniJam@users.noreply.github.com>
2026-04-06 13:37:59 -0500

    Adjusted Checkout page
    -  You must include the question number and the question instructions as comments above the js code
    
    Stuff you have to wait till jaydon push changes:
    - create invoice popup after confirm from alert (alert is already implemented)
    - create invoice page for user which shows all invoices

--- Codes/Javascript/mainScript.js
+++ Codes/Javascript/mainScript.js
@@ -1,6 +1,7 @@
 // saving username and login status in session storage for use across pages, will be cleared when browser is closed
 var username = sessionStorage.getItem("username") || "";
 var loggedIn = sessionStorage.getItem("loggedIn") === "true";
+
 let usernameDiv = null;
 
 // Header and nav elements for dynamic updates based on login status
